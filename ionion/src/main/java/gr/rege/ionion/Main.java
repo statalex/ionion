9cf82d856a230b3c4ff6d6fcae2b4fc7755a64f8
Stathis Alexopoulos <stathis@alcinia.net>
2018-04-12 18:10:12 +0300

    Some more development

--- ionion/src/main/java/gr/rege/ionion/Main.java
+++ ionion/src/main/java/gr/rege/ionion/Main.java
@@ -1,24 +1,42 @@
 package gr.rege.ionion;
 
-import gr.rege.ionion.info.DomainInfo;
+import java.io.File;
+
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.configuration2.CompositeConfiguration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gr.rege.ionion.helper.ExtCommandLine;
+import gr.rege.ionion.helper.PleskEnv;
 import gr.rege.ionion.info.SubscriptionInfo;
 
-public class Main {
+public class Main 
+{
+	static Logger logger = LoggerFactory.getLogger( Main.class);
+	
 
 	public static void main(String[] args) throws Exception 
 	{
-		String host = "";
-		String login = "";
-		String password = "";
+		PleskEnv env = PleskEnv.get(args);
 		
-		//hello
-
-		PleskApiClient client = new PleskApiClient(host);
-		client.setCredentials(login, password);
 		
-//		DomainInfo info = new DomainInfo();
-//		info.parse(client, "a-sakkali.gr");
-		new SubscriptionInfo().parse(client, "a-sakkali.gr");
+		switch( env.mode)
+		{
+			case DOMAIN :
+				new DomainChecker(env).check();
+				break;
+			case MBOX :
+				break;
+		}
 	}
 
+
+	
 }
--- ionion/src/main/java/gr/rege/ionion/info/SubscriptionInfo.java
+++ ionion/src/main/java/gr/rege/ionion/info/SubscriptionInfo.java
@@ -3,6 +3,7 @@ package gr.rege.ionion.info;
 import java.io.StringReader;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.velocity.VelocityContext;
 import org.dom4j.Branch;
@@ -12,13 +13,18 @@ import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
 import gr.rege.ionion.PleskApiClient;
-import gr.rege.ionion.helper.ParseUtil;
+import gr.rege.ionion.helper.PleskUtil;
+import gr.rege.ionion.helper.XmlMap;
 
 public class SubscriptionInfo 
 {
+	public static int d;
+	public int diskSpace;
+	public int mailboxQuota;
+	
 	public void parse( PleskApiClient client, String name) throws Exception
 	{
-		ParseUtil util = new ParseUtil();
+		PleskUtil util = new PleskUtil();
 		VelocityContext ctx = new VelocityContext();
 		ctx.put("name", name);
 		String response = client.request( util.getRequest("subscription_info_req", ctx));
@@ -28,51 +34,15 @@ public class SubscriptionInfo
 		SAXReader reader = new SAXReader();
 		Document document = reader.read( strReader );
 		util.setCurNode( document.selectSingleNode("/packet/webspace/get/result/data/limits" ));
-//		Node node = document.selectSingleNode("/packet/webspace/get/result/data/limits" );
 		List<Node> nodeList = util.getNodeList("limit");
-//		System.out.println( nodeList.size());
+		XmlMap map = new XmlMap();
 		for( Node node: nodeList)
 		{
-//			System.out.println("============================");
-			switch( node.getNodeType())
-			{
-				case Node.ELEMENT_NODE :
-					Element elm = (Element)node;
-//					System.out.println( "** "+elm.nodeCount());
-					String subName="";
-					String subValue="";
-					for (Iterator iterator = elm.nodeIterator(); iterator.hasNext();) 
-					{
-						Node sub = (Node) iterator.next();
-//						System.out.println("["+sub.getName()+"="+sub.getText()+"]");
-//						System.out.println("{"+sub.asXML()+"}");
-						if( sub.getName() != null)
-						{
-//							System.out.println("<"+sub.getName()+"="+sub.getText()+">");
-							if( sub.getName().compareTo("name")==0)
-								subName=sub.getText();
-							if( sub.getName().compareTo("value")==0)
-								subValue=sub.getText();
-						}
-					}
-					System.out.println("("+subName+"="+subValue+")");
-//					System.out.println( elm.asXML());
-					break;
-				case Node.ATTRIBUTE_NODE :
-					break;
-			}
-//			System.out.println(node.getName()+"="+node.getStringValue());
+			if( node.getNodeType() == Node.ELEMENT_NODE)
+				map.putNamedValue( (Element)node);
 		}
-		System.out.println( util.getCurNode());
-//		System.out.println( util.asString("disk_space"));
-//		status=util.asString( "status");
-//		id=util.asInt( "id");
-//		util.setCurNode("data/gen_info");
-//		name=util.asString("name");
-//		ipAddress=util.asString("dns_ip_address");
-//		realSize=util.asInt("real_size");
-//		util.setCurNode( document.selectSingleNode("/packet/webspace/get/result/data/disk_usage" ));
-		
+		diskSpace = map.getInt("disk_space");
+		mailboxQuota = map.getInt("mbox_quota");
 	}
 
 }
